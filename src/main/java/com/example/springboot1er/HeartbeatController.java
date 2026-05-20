9a59c3017abd2960714c16f5377906b7a1ef86d7
charlotte pinoit <charlotte.pinoit@le-campus-numerique.fr>
2026-05-20 11:25:29 +0200

    ajout endpoint GET /heartbeat

---
+++ src/main/java/com/example/springboot1er/HeartbeatController.java
@@ -0,0 +1,18 @@
+package com.example.springboot1er;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HeartbeatController {
+
+    @Autowired
+    private HeartbeatSensor heartbeatSensor;
+
+    @GetMapping("/heartbeat")
+    public int heartbeat() {
+        return heartbeatSensor.get();
+    }
+
+}
---
+++ src/main/java/com/example/springboot1er/HeartbeatSensor.java
@@ -0,0 +1,5 @@
+package com.example.springboot1er;
+
+public interface HeartbeatSensor {
+    int get();
+}
