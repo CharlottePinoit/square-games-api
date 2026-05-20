2537728ec4b20f4004a301b24fac7002388f28ce
charlotte pinoit <charlotte.pinoit@le-campus-numerique.fr>
2026-05-20 10:24:27 +0200

    premier commit

---
+++ src/main/java/com/example/springboot1er/Springboot1erApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot1er;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1erApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot1erApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot1er/Springboot1erApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot1er;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot1erApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
